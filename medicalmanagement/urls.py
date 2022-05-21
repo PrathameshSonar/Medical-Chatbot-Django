c516671d8a144ebf8533792f13302bf4501aa27b
PrathameshSonar <PRATHAMESH@PRATHAMESH>
2022-05-21 10:04:38 +0530

    UI changed

--- medicalmanagement/urls.py
+++ medicalmanagement/urls.py
@@ -7,15 +7,12 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-
     path('customer/',include('customer.urls')),
     path('',views.home_view,name=''),
     path('logout', LogoutView.as_view(template_name='medical/logout.html'),name='logout'),
     path('aboutus/', views.aboutus_view),
     #path('contactus', views.aboutus_view),
     path('afterlogin', views.afterlogin_view,name='afterlogin'),
-   
     path('adminlogin', LoginView.as_view(template_name='medical/adminlogin.html'),name='adminlogin'),
     path('bmi/', include("bmi.urls")),
 
